2b06f713dd06aa0e881c4086e8313a03ace9d277
fauzanmuh <f4uz4n.oioi@gmail.com>
2021-12-16 18:57:43 +0700

    Update

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -37,10 +37,10 @@
         @yield("content")
 
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy; 2021 <a href="http://adminlte.io">Muhammad Fauzan</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.0.0-rc.5
+                <b>Version</b> 1.0
             </div>
         </footer>
 
